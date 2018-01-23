4f0b508279ea3e8e6c3abb22ea980ee978d14516
cenesbitt3 <cenesbitt3@gmail.com>
2018-01-23 14:33:36 -0600

    fixed warping of grapple

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Grapple : MonoBehaviour {
 
     public Rigidbody player;
+
     public Transform rope;
     public Collider hookCollider;
     bool active;
     Rigidbody attachedRB;
+    private Transform anchor;
 
+    private void Update()
+    {
+        if (this.anchor != null)
+        {
+            this.transform.position = anchor.transform.position;
+            this.transform.rotation = anchor.transform.rotation;
+        }
+    }
     void FixedUpdate()
     {
         if (active)
@@ -26,6 +36,11 @@ public class Grapple : MonoBehaviour {
     {
         if (active)
         {
+            anchor = new GameObject("Grapple_Anchor").transform;
+            anchor.transform.position = this.transform.position;
+            anchor.transform.rotation = this.transform.rotation;
+            anchor.transform.parent = collision.transform;
+            this.anchor = anchor.transform;
             if (collision.gameObject.CompareTag("Harpoon"))
             {
                 player.GetComponent<Player>().Retract();
@@ -33,7 +48,7 @@ public class Grapple : MonoBehaviour {
             }
             Deactivate();
             player.gameObject.GetComponent<Player>().attached = true;
-            transform.parent = collision.gameObject.transform;
+            //transform.parent = collision.gameObject.transform;
 
             Transform otherRoot = collision.transform;
             while(otherRoot.parent != null)
@@ -43,10 +58,12 @@ public class Grapple : MonoBehaviour {
             attachedRB = otherRoot.GetComponent<Rigidbody>();
         }
         
+
     }
 
     public void Detach()
     {
+        Destroy(anchor.gameObject);
         attachedRB = null;
         active = false;
     }
