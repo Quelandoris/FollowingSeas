ab4f4ccf548db523cd79941d691c57a7c1014844
Grant <notalizard@users.noreply.github.com>
2018-01-22 23:51:29 -0600

    Merged Connor's changes into master

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+    //float chaseLength = 15;
+    //float scrollSpeed = -6;
+
+    public GameObject UnderwaterEffect;
+
+    LayerMask UnPhaseableLayers;
+    public LayerMask waterLayer;
+
+    void Start()
+    {
+        UnPhaseableLayers = ~(waterLayer);
+        
+        UnderwaterEffect.SetActive(false);
+    }
+    private void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+    private void Update()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+        if(Physics.Linecast(transform.parent.position,desiredCameraPos,out hit,UnPhaseableLayers))
+        {
+            distance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit, waterLayer))
+        {
+            UnderwaterEffect.SetActive(true);
+        }
+        else
+        {
+            UnderwaterEffect.SetActive(false);
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+        
+
+    }
+
+
+}
