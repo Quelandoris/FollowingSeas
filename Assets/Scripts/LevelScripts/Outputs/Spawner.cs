208227bf718405f8d428774fb9cc19d6f84d513d
Grant Barbee <grantcbarbee@users.noreply.github.com>
2018-02-02 16:12:20 -0600

    Added new level scripts

---
+++ Assets/Scripts/LevelScripts/Outputs/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : Toggleable {
+    public GameObject primaryObject;
+    public GameObject secondaryObject;
+
+	// Use this for initialization
+	void Start () {
+        if(primaryObject != null)
+        {
+            primaryObject.SetActive(false);
+        }
+        if(secondaryObject != null)
+        {
+            secondaryObject.SetActive(false);
+        }
+	}
+	
+    public override void Primary()
+    {
+        primary = true;
+        if (primaryObject != null)
+        {
+            GameObject newObj = Instantiate(primaryObject);
+            newObj.SetActive(true);
+        }
+    }
+
+    public override void Secondary()
+    {
+        primary = false;
+        if(secondaryObject != null)
+        {
+            GameObject newObj = Instantiate(secondaryObject);
+            newObj.SetActive(true);
+        }
+    }
+
+    public override void Toggle()
+    {
+        BasicToggle();
+    }
+}
