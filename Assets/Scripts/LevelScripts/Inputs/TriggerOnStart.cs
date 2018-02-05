1f2f67bab06e94c36334bcd212a5a8f0b6a027f3
Grant Barbee <grantcbarbee@users.noreply.github.com>
2018-02-05 17:39:57 -0600

    Added example log spawner in sandbox

---
+++ Assets/Scripts/LevelScripts/Inputs/TriggerOnStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnStart : MonoBehaviour {
+    public Toggleable objectToAffect;
+    public Actions actionType;
+
+	// Use this for initialization
+	void Start () {
+        switch (actionType)
+        {
+            case Actions.PRIMARY:
+                objectToAffect.Primary();
+                break;
+            case Actions.SECONDARY:
+                objectToAffect.Secondary();
+                break;
+            case Actions.TOGGLE:
+                objectToAffect.Toggle();
+                break;
+        }
+	}
+}
