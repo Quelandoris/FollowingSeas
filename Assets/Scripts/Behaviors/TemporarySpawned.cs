1b6440127091deac1a0393deec43b56349938fd4
Grant <notalizard@users.noreply.github.com>
2018-03-26 01:52:32 -0500

    Added spawners that expire

---
+++ Assets/Scripts/Behaviors/TemporarySpawned.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporarySpawned : MonoBehaviour {
+
+    public TemporaryObjectSpawner spawner;
+    public float lifespan;
+    float timeAlive = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeAlive += Time.deltaTime;
+        if(timeAlive >= lifespan)
+        {
+            spawner.Secondary();
+            Destroy(this.gameObject);
+        }
+	}
+
+}
---
+++ Assets/Scripts/LevelScripts/Outputs/TemporaryObjectSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryObjectSpawner : Toggleable {
+
+    public Transform spawnPoint;
+    public GameObject spawnObject;
+
+	// Use this for initialization
+	void Start () {
+        Secondary();
+	}
+
+    public override void Primary()
+    {
+        if (!primary)
+        {
+            primary = true;
+            GameObject newObj = Instantiate(spawnObject, spawnPoint.position, spawnPoint.rotation);
+            newObj.GetComponent<TemporarySpawned>().spawner = this;
+        }
+    }
+
+    public override void Secondary()
+    {
+        primary = false;
+    }
+
+    public override void Toggle()
+    {
+        //Do Nothing
+    }
+}
