3dc470f7c7261b6a3db268a87ea659fa6cb10eae
Hudson Bielstein <hbielstein@gmail.com>
2018-04-20 18:21:04 -0500

    Finished StormHaven

---
+++ Assets/ee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ee : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("IceIceBabylon");
+        }
+    }
+}
